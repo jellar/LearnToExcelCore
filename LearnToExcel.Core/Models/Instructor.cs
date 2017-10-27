dcf0d6d3fcdcaa93cc1857850f9c8cb7d79227c4
jellar <jella.rajesh@gmail.com>
2017-10-27 16:05:53 +0100

    Add files via upload

---
+++ LearnToExcel.Core/Models/Instructor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnToExcel.Core.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0: yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Start Date")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string Fullname => $"{FirstName} {Surname}";
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
